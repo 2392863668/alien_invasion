ce457d44d473eba1de9b1dc2445a896aacd7b1de
2392863668 <2392863668@qq.com>
2020-10-18 22:08:02 +0800

    重构子弹发射

--- game_functions.py
+++ game_functions.py
@@ -16,9 +16,14 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.move_left = True
     elif event.key == pygame.K_SPACE:
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果没有达到限制就发射一颗子弹"""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ai_settings, screen, ship, bullets):
