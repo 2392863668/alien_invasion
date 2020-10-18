e011fde2355e5aa0ac6e21b1a6a493ca5cb39493
2392863668 <2392863668@qq.com>
2020-10-18 20:47:21 +0800

    控制飞船移动速度

--- ship.py
+++ ship.py
@@ -9,9 +9,10 @@ import pygame
 
 
 class Ship:
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船图像并获取其外接矩阵
         self.image = pygame.image.load("images/ship.bmp")
@@ -21,6 +22,9 @@ class Ship:
         # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # 在飞船属性 center 中存储小数值
+        self.center = float(self.rect.centerx)
 
         # 移动标志
         self.move_right = False
@@ -33,6 +37,8 @@ class Ship:
     def update(self):
         """根据移动标识调整飞船位置"""
         if self.move_right:
-            self.rect.centerx += 1
-        elif self.move_left:
-            self.rect.centerx -= 1
+            self.center += self.ai_settings.ship_speed_factor
+        if self.move_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
