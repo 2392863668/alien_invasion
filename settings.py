823b720b6d76619726277a563a75f294eb1f3f5f
lukaiyi <lukaiyi@qq.com>
2020-10-19 18:10:33 +0800

    调整窗口大小以适应分辨率小的屏幕

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
 
     def __init__(self):
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 2
 
