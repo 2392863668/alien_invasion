a9a736d8d84430a47b883114a69225e9a87c7fe3
2392863668 <2392863668@qq.com>
2020-11-03 20:40:24 +0800

    在屏幕上绘制按钮

--- game_functions.py
+++ game_functions.py
@@ -67,13 +67,16 @@ def change_fleet_direction(ai_settings, aliens):
     ai_settings.fleet_direction *= -1
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """每次循环时都重绘屏幕"""
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+    # 如果游戏处于非活动状态，就绘制 Play 按钮
+    if not stats.game_active:
+        play_button.draw_button()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
