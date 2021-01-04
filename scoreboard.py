4d745b71074549959af82fab597d3b08839f159b
lukaiyi <lukaiyi@qq.com>
2021-01-04 11:11:47 +0800

    显示剩余飞船数目

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,7 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 
 class Scoreboard():
@@ -19,6 +22,7 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """将得分转换成一幅渲染的图像"""
@@ -52,9 +56,18 @@ class Scoreboard():
         self.level_rect.right = self.screen_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
+    def prep_ships(self):
+        """显示还剩下多少飞船"""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
     def show_score(self):
         """在屏幕上显示得分"""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
-
+        self.ships.draw(self.screen)
